761baddd73ac49f78a2bf99c5e9ba76d4dd294d9
Richard Brown <78174789+richb2010@users.noreply.github.com>
2022-07-25 12:24:50 -0600

    Update main.js

--- client/main.js
+++ client/main.js
@@ -1,35 +1,37 @@
+const { getFortune } = require("../server/controller");
+
 const complimentBtn = document.getElementById("complimentButton")
 
 const getCompliment = () => {
-    axios.get("http://127.0.0.1:5500/client/")
+    axios.get("http://localhost:4000/api/complimaent/")
         .then(res => {
             const data = res.data;
             alert(data);
     });
 };
-const getAdvice = () => {
-    axios.get("http://127.0.0.1:5500/client/")
-        .then(res => {
-            const data = res.data;
-            alert(data);
-    });
-};
-const getAdvice2 = () => {
-    axios.get("http://127.0.0.1:5500/client/")
-        .then(res => {
-            const data = res.data;
-            alert(data);
-    });
-};
-const getAdvice3 = () => {
-    axios.get("http://127.0.0.1:5500/client/")
-        .then(res => {
-            const data = res.data;
-            alert(data);
-    });
-};
-complimentBtn.addEventListener('click', getCompliment)
-AdviceBtn.addEventListener('click', getAdvice)
-AdviceBtn.addEventListener('click', getAdvice)
-AdviceBtn.addEventListener('click', getAdvice)
 
+complimentBtn.addEventListener('click',getCompliment)
+const fortunesBtn = document.getElementById("fortuneButton")
+const complimaentInput = document.getElementById("complimentInput")
+const addComplimentButton = document.getElementById("addComplimentButton")
+
+const getFortune = () => {
+    axios.get("http://localhost:4000/api/fortune/")
+        .then(res => {
+            const data = res.data;
+            alert(data);
+    });
+};
+const addCompliment = () => {
+    const complimaentInput = document.getElementById('complimentInput')
+    const body ={
+        compliment: complimaentInput.value
+    }
+    axios.post(`http://localhost:4000/api/compliment`, body)
+    .then(() => alert('compliment has been added'))
+    .catch ((err) => console.log(err) )
+}
+
+complimentBtn.addEventListener('click', getAlertData)
+fortuneBtn.addEventListener('click',getAlertData)
+addcomplimentBtn.addEventListener('click', addCompliment)
